49eda5cb5d0d9a6fb6d780bfb6e9766ba458074a
shivam <shivam.sharma@digitup.in>
2023-08-18 23:21:42 +0530

    f-c

--- schemas/blog.tsx
+++ schemas/blog.tsx
@@ -1,47 +1,45 @@
-// import { MdLocalOffer as TagIcon } from 'react-icons/md'; // This imports an icon for tags
-
 export default {
-  name: 'blogPost',
-  title: 'Blog Post',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 200,
+    name: 'blogPost',
+    title: 'Blog Post',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
       },
-    },
-    {
-      name: 'author',
-      title: 'Author',
-      type: 'string',
-    },
-    {
-      name: 'content',
-      title: 'Content',
-      type: 'array',
-      of: [{ type: 'block' }],
-    },
-    {
-      name: 'tags',
-      title: 'Tags',
-      type: 'array',
-      of: [
-        {
-          type: 'string',
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 200,
         },
-      ],
-    },
-  ],
-};
-
-
-
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'string',
+      },
+      {
+        name: 'content',
+        title: 'Content',
+        type: 'array',
+        of: [{ type: 'block' }],
+      },
+      {
+        name: 'tags',
+        title: 'Tags',
+        type: 'array',
+        of: [
+          {
+            type: 'string',
+          },
+        ],
+      },
+    ],
+  };
+  
+  
+  
